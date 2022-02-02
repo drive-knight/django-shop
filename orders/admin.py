60c93201be2c599f63c6739474b564ee04ddace5
drive-knight <yavatar15@yandex.ru>
2022-02-02 07:39:39 +0500

    expanded admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 
 class OrderItemInline(admin.TabularInline):
@@ -7,10 +9,17 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(
+                     reverse('orders:admin_order_detail', args=[obj.id])))
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+
+
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .models import Order
 from cart.cart import Cart
 from .tasks import order_created
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
 
 def order_create(request):
@@ -24,3 +27,9 @@ def order_create(request):
         form = OrderCreateForm()
     return render(request, 'orders/create.html',
                             {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
